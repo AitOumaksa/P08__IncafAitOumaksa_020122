c06e2c2fd26636c351ef23a8d9417c4934d3542e
Incaf Ait Oumaksa <hdidana.incaf@gmail.com>
2023-01-07 14:57:18 +0100

    Default Controller Test

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -11,13 +11,7 @@ class DefaultControllerTest extends WebTestCase
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
-
-        //$this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertEquals(302, $client->getResponse()->getStatusCode()); 
-        //$this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
-       
-
-       // $crawler = $client->followRedirect();
         $this->assertSame(0, $crawler->filter('html:contains("Bienvenue sur Todo List, l\'application vous permettant de gérer l\'ensemble de vos tâches sans effort !")')->count());
     }
 }
