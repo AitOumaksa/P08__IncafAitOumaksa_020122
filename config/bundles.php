c8eda3d0eef783b7e7753b16afe59e398801d63a
Incaf Ait Oumaksa <hdidana.incaf@gmail.com>
2023-01-27 14:23:37 +0100

    deleted old version

--- config/bundles.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-];
